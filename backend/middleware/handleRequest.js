33b08b82bc9ec1ce43426fa5c37c912916c564e7
Alex.X <yxiong@brandeis.edu>
2019-09-04 15:17:17 -0400

    fix:project structure

--- backend/middleware/handleRequest.js
+++
@@ -1,9 +0,0 @@
-function handleRequest( res, ctx ){
-    if(res.code === 0){
-        ctx.send(res)
-    } else {
-        ctx.status(400).json("Error: " + res.msg);
-    }
-}
-
-module.exports = handleRequest;
